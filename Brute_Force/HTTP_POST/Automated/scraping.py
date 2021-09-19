7a0f1db0d4068818f955d0fa34008ed0be3175ae
Shashwat-Shah44 <technologyenglish05@gmail.com>
2021-09-19 18:47:40 +0530

    Working With Automations

--- Brute_Force/HTTP_POST/Automated/scraping.py
+++ Brute_Force/HTTP_POST/Automated/scraping.py
@@ -1,3 +1,4 @@
+import sys
 from .important_static_functions import *
 
 
@@ -8,9 +9,17 @@ def main(url):
         print(success(f'Action: {action}, Method: {method}', 'yellow'))
         inputs = getting_specify_tags(form, 'input')
         tag_types, tag_names, submit = extracting_tags_attributes(inputs, 'type', 'name')
-        for types, names in zip(tag_types, tag_names):
-            print(success(f'Input Type: {types}, Input Name: {names}'))
-        try:
-            print(success(f'Submit Button Name: {submit[0][1]}'))
-        except IndexError:
-            print(error(f'Cannot Find The Submit Input Field Name'))
+        if len(tag_types) < 3 and len(tag_names) < 3:
+            for types, names in zip(tag_types, tag_names):
+                print(success(f'Input Type: "{types}", Input Name: "{names}"'))
+            try:
+                print(success(f'Submit Button Name: {submit[0][1]}'))
+            except IndexError:
+                print(error(f'Cannot Find The Submit Input Field Name'))
+        else:
+            field = 1
+            print(error(f'More Than 2 inputs Field Found, Please use Manual mode.'))
+            for types, names in zip(tag_types, tag_names):
+                print(error(f'Input Type: "{types}", Input Name: "{names}"' + colored(f"<= {field} Field", 'blue')))
+                field += 1
+            sys.exit(0)
