d16a716c9523997e2ed17ade6e2c6395714b51c0
Shashwat-Shah44 <technologyenglish05@gmail.com>
2021-09-20 20:17:05 +0530

    Working With Automations

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from Brute_Force.HTTP_POST.Automated.start import *
+from pyfiglet import Figlet
+
+
+def banner():  # Simple Banner Of The Program.
+    fix_color_output()  # Fixing Color Output in windows and linux both.
+    return colored(Figlet(font='standard').renderText('Venom Brute-Force'), 'blue')
+
 
 if __name__ == '__main__':
+    # python main.py -l "admin" -u "https://facebook.com/login" -p /passwords.txt -v "Login Failed"
+    print("\n\n" + str(banner()) + "\n\n")
     url = 'https://facebook.com/login'
     Start().run()
